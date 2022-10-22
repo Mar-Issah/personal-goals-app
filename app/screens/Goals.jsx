2998eade6c778ba1200dc6bcbdb1a9e0456e8264
Mar-Issah <masy370@gmail.com>
2022-10-22 14:10:14 -0400

    feat: implement the event handler for the input and button

--- app/screens/Goals.jsx
+++ app/screens/Goals.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
-const Goals = (props) => {
+const Goals = () => {
+  const [text, setText] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const handleInput = (inputText) => {
+    console.log(inputText);
+    setText(inputText);
+  };
+
+  const handleAddGoal = () => {
+    console.log(text);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Type your goal' style={styles.textInput} />
-        <Button title='ADD GOAL' color='#000' style={styles.button} />
+        <TextInput placeholder='Type your goal' style={styles.textInput} onChangeText={handleInput} />
+        <Button title='ADD GOAL' color='#000' style={styles.button} onPress={handleAddGoal} />
       </View>
       <View style={styles.goalContainer}>
         <Text style={styles.listText}>List of goals...</Text>
@@ -20,22 +31,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#d4d1cb',
-    padding: 30,
+    paddingTop: 50,
+    paddingHorizontal: 16, //padding left and right
   },
   inputContainer: {
     flexDirection: 'row',
-    height: 40,
+    flex: 1,
+    alignItems: 'center',
+    marginBottom: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
   },
   textInput: {
     width: '75%',
     backgroundColor: '#fff',
+    padding: 4,
   },
   goalContainer: {
-    marginTop: 20,
-    alignItems: 'center',
+    flex: 5,
   },
   listText: {
     fontWeight: 'bold',
+    width: '100%',
   },
 });
 
