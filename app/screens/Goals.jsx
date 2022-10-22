077401e72dd103ae38785f5c14562cae3b73df90
Mar-Issah <masy370@gmail.com>
2022-10-22 00:10:16 -0400

    feat: creastyle input and button

--- app/screens/Goals.jsx
+++ app/screens/Goals.jsx
@@ -5,9 +5,9 @@ import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 const Goals = (props) => {
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder='Type your goal' />
-        <Button title='ADD GOAL' />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Type your goal' style={styles.textInput} />
+        <Button title='ADD GOAL' color='#000' style={styles.button} />
       </View>
       <View>
         <Text>List of goals...</Text>
@@ -19,9 +19,16 @@ const Goals = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#d4d1cb',
+    padding: 30,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    height: 40,
+  },
+  textInput: {
+    width: '75%',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
