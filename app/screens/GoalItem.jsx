7f6406b0db0d4057418de8a2b4fb62fae6a7e742
Mar-Issah <masy370@gmail.com>
2022-10-23 13:23:37 -0400

    feat: add stlye to pressable in ios

--- app/screens/GoalItem.jsx
+++ app/screens/GoalItem.jsx
@@ -8,20 +8,30 @@ const GoalItem = ({ item, onDeleteGoal, id }) => {
   // console.log(item);
 
   return (
-    <Pressable onPress={deleteGoal}>
-      <View style={styles.goalItem}>
-        <Text>{item.text}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#ddd' }}
+        onPress={deleteGoal}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.text}>{item.text}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 5,
-    padding: 5,
     borderRadius: 6,
     backgroundColor: '#de84f5',
+    //padding: 5,
+  },
+  text: {
+    padding: 5, //move the padding here so the ripple effect can fill the container
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
 
